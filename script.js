d73c6f34385c44d032a565f388ee78ac3b01661a
Andrew Chang <acchang4@gmail.com>
2022-04-22 10:15:21 -0400

    done

--- script.js
+++ script.js
@@ -25,11 +25,57 @@ function getFetch(){
     else Topic = noSpacesTopic
     console.log(Topic)
 
-fetch(`https://api.artic.edu/api/v1/artworks/search?q=${Topics}&query[term][date_start]=${year}`)
+    let api_link
 
+fetch(`https://api.artic.edu/api/v1/artworks/search?q=${Topic}&query[term][date_start]=${year}`)
   .then(res => res.json()) 
   .then(data => {
+
+    if (data.data.length < 1){
+      document.getElementById('title').innerText = "No Results"
+      document.getElementById('artist').innerText = ""
+      document.querySelector('img').src = ""}
+
+    else {
       console.log(data)
+      let number = Math.floor(Math.random() * data.data.length)
+      api_link = data.data[number].api_link
+      console.log("api:" + data.data[0].api_link)
+
+// 1980, woman gets Cindy Sherman
+
+      fetch(`${api_link}`)
+        .then(res => res.json()) 
+        .then(data => {          
+          document.getElementById('title').innerText = ""
+          document.getElementById('title').innerText = data.data.title
+          document.getElementById('artist').innerText = ""
+          document.getElementById('artist').innerText = data.data.artist_display
+          document.querySelector('img').src =
+          `https://www.artic.edu/iiif/2/${data.data.image_id}/full/843,/0/default.jpg`
+
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        })
+    }
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      })
+}
+
+
+
+      // console.log(data.data[0].api_link.title)
+      // console.log(data.data[0].api_link.artist_display)
+      // console.log(data.data[0].api_link.image_id)
+
+      // get first item out of array (later randomize for length of array)
+      // get api link, ie https://api.artic.edu/api/v1/artworks/97434
+      // from api link, get title, artist_display, image_id	
+      // https://www.artic.edu/iiif/2/{identifier}/full/600,/0/default.jpg
+
 
     //   const lineBreak = document.createElement('br');
     //   document.getElementById('incidentlist').appendChild(lineBreak);
@@ -47,20 +93,18 @@ fetch(`https://api.artic.edu/api/v1/artworks/search?q=${Topics}&query[term][date
     //       document.getElementById('incidentlist').appendChild(lineBreak)
     //     }
     //   }
-  })
-  .catch(err => {
-      console.log(`error ${err}`)
-  });
-}
+//   })
+//   .catch(err => {
+//       console.log(`error ${err}`)
+//   });
 
+// }
 
-
-
-// fetch(`https://api.artic.edu/api/v1/artworks`)
-// .then(res => res.json()) 
-// .then(data => {
-//     console.log(data)
-// })
-// .catch(err => {
-//     console.log(`error ${err}`)
-// });
+fetch(`https://api.artic.edu/api/v1/artworks/193864`)
+.then(res => res.json()) 
+.then(data => {
+    console.log(data)
+})
+.catch(err => {
+    console.log(`error ${err}`)
+});
