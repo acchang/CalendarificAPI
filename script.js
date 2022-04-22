048492b35c470275e78082f3b6cdb68f87d7ffd2
Andrew Chang <acchang4@gmail.com>
2022-04-22 10:23:35 -0400

    cleaned up scraps from script

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-
-// within parameters, need to get the image id and pull it from another DB
-// do a full text search and get the first object and print it?
-// return it to https://www.artic.edu/iiif/2/6884e725-f7ae-cb1c-853a-201fc4fe5630/full/600,/0/default.jpg
-
-
-
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
@@ -65,46 +58,3 @@ fetch(`https://api.artic.edu/api/v1/artworks/search?q=${Topic}&query[term][date_
       })
 }
 
-
-
-      // console.log(data.data[0].api_link.title)
-      // console.log(data.data[0].api_link.artist_display)
-      // console.log(data.data[0].api_link.image_id)
-
-      // get first item out of array (later randomize for length of array)
-      // get api link, ie https://api.artic.edu/api/v1/artworks/97434
-      // from api link, get title, artist_display, image_id	
-      // https://www.artic.edu/iiif/2/{identifier}/full/600,/0/default.jpg
-
-
-    //   const lineBreak = document.createElement('br');
-    //   document.getElementById('incidentlist').appendChild(lineBreak);
-    //   document.getElementById('incidentlist').innerText = "";
-
-    //   if (data.length < 1){
-    //     document.getElementById('incidentlist').innerText = "No Results"
-    //   }
-    //   else {
-    //     for (i=0; i<data.length; i++) {
-    //       // document.querySelector('h2').innerText += data[i].complaint_type + " : " + data[i].descriptor
-    //       // document.querySelector('h2').appendChild(lineBreak)
-
-    //       document.getElementById('incidentlist').innerText += data[i].complaint_type + " : " + data[i].descriptor
-    //       document.getElementById('incidentlist').appendChild(lineBreak)
-    //     }
-    //   }
-//   })
-//   .catch(err => {
-//       console.log(`error ${err}`)
-//   });
-
-// }
-
-fetch(`https://api.artic.edu/api/v1/artworks/193864`)
-.then(res => res.json()) 
-.then(data => {
-    console.log(data)
-})
-.catch(err => {
-    console.log(`error ${err}`)
-});
