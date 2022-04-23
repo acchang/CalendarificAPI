d3367dec6ecafb9527f548bb5bc8ce481d4bb458
Andrew Chang <acchang4@gmail.com>
2022-04-23 11:59:21 -0400

    first try

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
-  const initialYear = document.getElementById('year').value
-  const initialTopic = document.getElementById('Topic').value
+    let country = "US"
+    const month
+    const day
 
-  let year
-  let noSpacesYear = initialYear.split(" ")
-    if (noSpacesYear.length > 1) {
-      year = noSpacesYear.join('%20')}
-    else year = noSpacesYear
+    const pickerDate = document.getElementById('date').value
+    const splitDate = pickerDate.split('-');
 
-  let Topic
-  let noSpacesTopic = initialTopic.split(" ")
-    if (noSpacesTopic.length > 1) {
-      Topic = noSpacesTopic.join('%20')}
-    else Topic = noSpacesTopic
+    const year = splitDate[0]
+    console.log(year)
 
-    let api_link
+    if (splitDate[1].charAt(0) = "0"){month = splitDate[1].charAt(1)}
+      else month = splitDate[1]
+    console.log(month)
+  
+    if (splitDate[2].charAt(0) = "0"){month = splitDate[2].charAt(1)}
+    else day = splitDate[2]
+    console.log(day)
 
-fetch(`https://api.artic.edu/api/v1/artworks/search?q=${Topic}&query[term][date_start]=${year}`)
+fetch(`https://calendarific.com/api/v2/holidays?api_key=b75fdd08b1fa9eb16e4821057eef4c83ca861f77&country=${country}&year=${year}&month=${month}&day=${day}`)
+// https://calendarific.com/api/v2/holidays?api_key=b75fdd08b1fa9eb16e4821057eef4c83ca861f77&country=US&year=2019&month=1&day=23
   .then(res => res.json()) 
   .then(data => {
 
     if (data.data.length < 1){
-      document.getElementById('title').innerText = "No Results"
-      document.getElementById('artist').innerText = ""
-      document.querySelector('img').src = ""}
+      document.getElementById('name').innerText = "No Results"
+      document.getElementById('description').innerText = ""
 
     else {
       console.log(data)
@@ -48,6 +49,7 @@ fetch(`https://api.artic.edu/api/v1/artworks/search?q=${Topic}&query[term][date_
             console.log(`error ${err}`)
         })
     }
+    
       })
       .catch(err => {
           console.log(`error ${err}`)
